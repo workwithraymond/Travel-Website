9dee458cf124fc3044829f693b92426bf042559c
workwithraymond <workwithraymond01@gmail.com>
2023-07-20 19:25:29 -0400

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import logo from '../images/logo.svg'
+import SocialLinks  from './SocialLink';
+import PageLinks from './PageLinks';
+import { socialLinks } from '../data';
+
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="backroads" />
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>
+       
+        <PageLinks parentClass='nav-links' itemClass='nav-link'/>
+
+        <ul className='nav-icons'>
+          {socialLinks.map((link) => {
+            return  <SocialLinks key={link.id} {...link} itemClass='nav-icon'/>;
+            
+          })}
+
+        </ul>
+        
+
+        
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
---
+++ src/components/SocialLink.js
@@ -0,0 +1,15 @@
+const SocialLinks = ({ href, icon, itemClass }) => {
+ 
+            return (
+              <li >
+                <a href={href} target='_blank' rel='noreferrer' className={itemClass}>
+                  <i className= {icon}></i>
+                </a>
+                
+              </li>
+            
+          )
+          };
+
+      
+export default SocialLinks
